55d05051d758a8de73a3111b0d4155294d01d82d
Rafal Kasa <rafal.kasa@makit.pl>
2019-12-28 13:22:02 +0100

    change postgres sql version

--- pg-backup-restore-azure/install.sh
+++ pg-backup-restore-azure/install.sh
@@ -5,6 +5,7 @@ set -e
 
 apt-get update
 apt-get install -y wget apt-transport-https
+apt-get install wget ca-certificates
 
 wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | apt-key add -
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ jessie-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
@@ -12,7 +13,7 @@ sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ jessie-pgdg main" >> /
 apt-get update
 
 # install pg_dump
-apt-get install -y postgresql
+apt-get install -y postgresql-11 postgresql-contrib
 
 # install azure cli
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
